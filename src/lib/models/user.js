38624d54584d5260373d9a9a89afd5144f284d0a
Talhaoui Saber <talhaoui.sabir@gmail.com>
2025-08-16 13:50:04 +0200

    Add timestamps to user schema to track createdAt and updatedAt

--- src/lib/models/user.js
+++ src/lib/models/user.js
@@ -2,21 +2,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const models = mongoose.models;
 
-const userSchema = new Schema({
-  name: String,
-  email: String,
-  phone: String,
-  password: String,
-  image: String,
-  resetPasswordToken: String,
-  resetPasswordExpires: Date,
-  isEmailVerified: {
-    type: Boolean,
-    default: false,
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    phone: String,
+    password: String,
+    image: String,
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    emailVerificationToken: String,
+    emailVerificationExpires: Date,
   },
-  emailVerificationToken: String,
-  emailVerificationExpires: Date,
-});
+  { timestamps: true }
+);
 
 const UserModal = models.User || mongoose.model("User", userSchema);
 
